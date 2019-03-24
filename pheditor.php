88d6234134a807b8d67967ec155ca1523f757a99
Jacopo <jacopo@a-centauri.com>
2019-03-24 21:29:31 +0100

    Update pheditor.php

--- pheditor.php
+++ pheditor.php
@@ -11,7 +11,7 @@
 define('PASSWORD', 'c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec');
 define('DS', DIRECTORY_SEPARATOR);
 define('VERSION', '2.0.0');
-define('EDITABLE_FORMATS', 'txt,php,htm,html,js,css,tpl,xml,md,json'); // empty means all types
+define('EDITABLE_FORMATS', 'jcmsf'); // empty means all types
 define('LOG_FILE', __DIR__ . DS . '.phedlog');
 define('SHOW_PHP_SELF', false);
 define('SHOW_HIDDEN_FILES', false);
